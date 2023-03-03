3747a38b496f514e927cc1cfc0c79d14c7c12124
Axel Quintana <lucasaxelqg15@gmail.com>
2023-03-03 07:01:20 -0300

    descubriendo algunas cositas

--- javascript/app.js
+++ javascript/app.js
@@ -293,3 +293,52 @@ const HIDEMUSEUM = document.querySelector(".hide-museum")
 CUBE.addEventListener("click",()=>{
     HIDEMUSEUM.classList.toggle("show-museum")
 })
+
+// CHAT GPT SOLUTIONS
+
+let myImage = document.querySelector("#myImage");
+let isDragging = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+myImage.addEventListener("mousedown", dragStart);
+myImage.addEventListener("mouseup", dragEnd);
+myImage.addEventListener("mousemove", drag);
+
+function dragStart(e) {
+  initialX = e.clientX - xOffset;
+  initialY = e.clientY - yOffset;
+
+  if (e.target === myImage) {
+    isDragging = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  isDragging = false;
+}
+
+function drag(e) {
+  if (isDragging) {
+    e.preventDefault();
+
+    currentX = e.clientX - initialX;
+    currentY = e.clientY - initialY;
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, myImage);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
